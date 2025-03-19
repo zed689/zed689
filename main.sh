60b76db36a9617ab0bb28bc63d587eb14a2a0dc9
zed689 <synnaulaid@gmail.com>
2025-03-19 21:13:11 +0000

    fix

--- main.sh
+++ main.sh
@@ -7,6 +7,8 @@ sudo apt update && sudo apt install -y build-essential git
 # Clone dan build mkp224o
 git clone https://github.com/cathugger/mkp224o.git
 cd mkp224o
+./autogen.sh
+./configure
 make
 sudo cp mkp224o /usr/local/bin/
 cd ..
